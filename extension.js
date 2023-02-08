44b1b3da00ffe6083a8b08a1420f36bb3c7d83f0
DEF <38461745+DarthEternalFaith@users.noreply.github.com>
2023-02-08 15:50:38 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -8,8 +8,8 @@ const { create_AlignFnc } = require('./js/create_vsc_act');
 function activate(context) {
 	var align__def=create_AlignFnc(),
 		align__ex_special_symbols=create_AlignFnc([[/^(?!:[+-\[\{}]])([^+-])/,' $1']]);
-	let disposable__def = vscode.commands.registerCommand('align-text-like-table.align', align__def()),
-		disposable__ex_special_symbols=vscode.commands.registerCommand('align-text-like-table.align_ex_special_symbols', align__ex_special_symbols());
+	let disposable__def = vscode.commands.registerCommand('align-text-like-table.align', align__def),
+		disposable__ex_special_symbols=vscode.commands.registerCommand('align-text-like-table.align_ex_special_symbols', align__ex_special_symbols);
 		
 	context.subscriptions.push(disposable__def,disposable__ex_special_symbols);
 }
